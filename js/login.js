e607ee34b917c3d9c7d0f6624f20f97b9150c95f
evewinny <evewinny10@gmail.com>
2021-02-20 19:18:23 +0800

    nyerahma

--- js/login.js
+++ js/login.js
@@ -1,34 +1,29 @@
-$('#tombol').click(function (e) {
-  e.preventDefault();
-  var email = $('#email').val();
-  var password = $('#password').val();
+$(document).ready(function () {
+  $('#tombol').click(function (e) {
+    e.preventDefault();
+    var email = $('#email').val();
+    var password = $('#password').val();
 
-  $.ajax({
-    url:'https://51018005.p-web.click/learnapp/member/login',
-    method:'POST',
-    data:{
-      'email': email,
-      'password': password
-    },
-    dataType:'json',
+    $.ajax({
+      url:'https://51018005.p-web.click/learnapp/member/login',
+      method:'POST',
+      data:{
+        'email': email,
+        'password': password
+      },
+      dataType:'json',
       success: function (e) {
-      console.log(e);
-      if (e.message=='success') {
-        // document.cookie="asdsafaf";
-        var b=`$('<input/>').attr({
-                             type: "button",
-                             id: "btnr",
-                             onclick="window.location='https://51018005.p-web.click/nihongo/index.html'"})
-                        `;
-                        $("body").append(b);
-                        $("#btnr").click();
-        document.location="https://51018005.p-web.click/nihongo/index.html";
-      } else {
-        alert("Email/Password anda salah");
+        console.log(e);
+        if (e.message=='success') {
+          // document.cookie="asdsafaf";
+          window.location="https://51018005.p-web.click/nihongo/index.html";
+        } else {
+          alert("Email/Password anda salah");
+        }
+      },
+      error: function (e) {
+        console.log(e);
       }
-    },
-    error: function (e) {
-      console.log(e);
-    }
+    });
   });
 });
